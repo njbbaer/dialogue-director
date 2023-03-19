55e8f8ef4f1a9a5d90e1c44de50db97ccaa70b3b
Nate Baer <njbbaer@gmail.com>
2023-03-19 01:29:02 -0700

    Fix custom model parameter

--- src/llm.py
+++ src/llm.py
@@ -6,12 +6,21 @@ class LLM:
     def __init__(self, config):
         self.config = config
 
+    def _parameters(self):
+        return {
+            **self.DEFAULT_PARAMETERS,
+            **self.config.parameters
+        }
+
 
 class OpenAI(LLM):
+    DEFAULT_PARAMETERS = {
+        'model': 'gpt-3.5-turbo',
+    }
+
     @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
     def complete(self, messages: str) -> str:
         return openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            **self.config.parameters,
+            **self._parameters(),
             messages=messages,
         )['choices'][0]['message']['content']
